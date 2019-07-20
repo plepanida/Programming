495da4536422d4ba464963f54c6216c8b6f3bd13
plepanida <52452594+plepanida@users.noreply.github.com>
2019-07-20 18:35:27 +0700

    Add files via upload

---
+++ Exercise4_Panida_J.py
@@ -0,0 +1,9 @@
+e=20
+g=30
+i=45
+c=25
+print("---------your score--------")
+print("Foundation English :",e)
+print("General Business:",g)
+print("Introduction to Computer Score:",i)
+print("Computer Programming:",c)
