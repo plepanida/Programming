27bed3c0647c5b327cb97a4d9941fad64beef68b
plepanida <52452594+plepanida@users.noreply.github.com>
2019-07-28 20:28:27 +0700

    Delete Exercise4_Panida_J.py

--- Exercise4_Panida_J.py
+++
@@ -1,9 +0,0 @@
-e=20
-g=30
-i=45
-c=25
-print("---------your score--------")
-print("Foundation English :",e)
-print("General Business:",g)
-print("Introduction to Computer Score:",i)
-print("Computer Programming:",c)
